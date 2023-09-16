b68cfb3e53222298e29f4f7090090ea7decfb9b8
gasoug <gabriel.sousago@gmail.com>
2023-09-16 20:31:48 -0300

    Projeto Integrador Petfun

---
+++ PTI/README.md
@@ -0,0 +1,2 @@
+# PI-8Trim
+Projeto Integrador - Desenvolvimento de Sistemas Orientado a Dispositivos Móveis e Baseados na Web
---
+++ PTI/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $("#form").submit(function(event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
+
+        // Obtenha os valores dos campos Email e Senha
+        var email = $("#email").val();
+        var senha = $("#senha").val();
+
+        localStorage.setItem("email", email);
+        localStorage.setItem("senha", senha);
+
+        // Faça algo com os valores, como enviá-los ao servidor via AJAX
+        console.log("Email: " + email);
+        console.log("Senha: " + senha);
+
+        // Opcional: Exiba uma mensagem de confirmação
+        //alert("Email: " + email + "\nSenha: " + senha);
+    });
+    $("#logar").click(function() {
+        // Obtenha os valores digitados pelo usuário
+        var loginDigitado = $("#login1").val();
+        var senhaDigitada = $("#senha1").val();
+
+        // Obtenha os valores do localStorage
+        var loginArmazenado = localStorage.getItem("email");
+        var senhaArmazenada = localStorage.getItem("senha");
+        //console.log(loginArmazenado, senhaArmazenada)
+        // Verifique se os valores coincidem
+        if (loginDigitado == loginArmazenado && senhaDigitada == senhaArmazenada) {
+            // Redirecione para a página de usuário
+            alert("Logado com sucesso");
+            window.location.href = "usuario.html";
+        } else {
+            // Exiba uma mensagem de erro (opcional)
+            alert("Login ou senha incorretos. Por favor, tente novamente.");
+            $("#login1").val('');
+            $("#senha1").val('');
+        }
+    });
+});
